b9d16442b6c9683ccb4d3f364f7c9c8045aa79ec
Thanos Loise <thanoslo@ga-f4.msk.21-school.ru>
2024-12-17 23:11:41 +0300

    Add facade & builder

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -1,7 +1,55 @@
 package pattern
 
+import "fmt"
+
 /*
-	Реализовать паттерн «фасад».
-Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Facade_pattern
+   Реализовать паттерн «фасад».
+   Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+   https://en.wikipedia.org/wiki/Facade_pattern
 */
+
+// Плюсы
+// 1. Изолирует клиентов от компонентов сложной системы.
+// 2. Предоставляет простой интерфейс для работы со сложной системой.
+// 3. Изменения в сложной системе не влияют на клиентов, если интерфйес остается неизменным.
+// Минусы
+// 1. Фасад может не предоставлять весь функционал сложной системы.
+
+// Database представляет сложную подсистему
+type Database struct{}
+
+func (d *Database) Start() {
+	fmt.Println("Database started.")
+}
+
+// Server представляет другую сложную подсистему
+type Server struct{}
+
+func (s *Server) Start() {
+	fmt.Println("Server started.")
+}
+
+// Facade предоставляет простой интерфейс для взаимодействия с подсистемами
+type Facade struct {
+	db     *Database
+	server *Server
+}
+
+// NewFacade создает новый экземпляр фасада
+func NewFacade() *Facade {
+	return &Facade{
+		db:     &Database{},
+		server: &Server{},
+	}
+}
+
+// Start запускает все подсистемы через фасад
+func (f *Facade) Start() {
+	f.db.Start()
+	f.server.Start()
+}
+
+// func main() {
+// 	facade := NewFacade()
+// 	facade.Start()
+// }
--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -5,3 +5,73 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+// Плюсы
+// Позволяет создавать объект поэтапно, вызывая методы строителя.
+// Улучшает читаемость кода, так как каждый метод строителя отвечает за установку одного параметра.
+// Минусы
+// Может затруднить внедрение зависимостей.
+// Увеличивает сложность кода из-за необходимости создания дополнительных классов и интерфейсов.
+// Может быть избыточным для простых объектов, где достаточно использовать конструктор с параметрами.
+
+// House представляет сложный объект, который будем создавать
+type House struct {
+	walls        string
+	roof         string
+	floors       int
+	garage       bool
+	swimmingPool bool
+}
+
+// HouseBuilder определяет интерфейс строителя
+type HouseBuilder interface {
+	SetWalls(walls string) HouseBuilder
+	SetRoof(roof string) HouseBuilder
+	SetFloors(floors int) HouseBuilder
+	SetGarage(garage bool) HouseBuilder
+	SetSwimmingPool(swimmingPool bool) HouseBuilder
+	Build() House
+}
+
+// ConcreteHouseBuilder реализует интерфейс HouseBuilder
+type ConcreteHouseBuilder struct {
+	house House
+}
+
+func NewHouseBuilder() *ConcreteHouseBuilder {
+	return &ConcreteHouseBuilder{}
+}
+
+func (b *ConcreteHouseBuilder) SetWalls(walls string) HouseBuilder {
+	b.house.walls = walls
+	return b
+}
+
+func (b *ConcreteHouseBuilder) SetRoof(roof string) HouseBuilder {
+	b.house.roof = roof
+	return b
+}
+
+func (b *ConcreteHouseBuilder) SetFloors(floors int) HouseBuilder {
+	b.house.floors = floors
+	return b
+}
+
+func (b *ConcreteHouseBuilder) SetGarage(garage bool) HouseBuilder {
+	b.house.garage = garage
+	return b
+}
+
+func (b *ConcreteHouseBuilder) SetSwimmingPool(swimmingPool bool) HouseBuilder {
+	b.house.swimmingPool = swimmingPool
+	return b
+}
+
+func (b *ConcreteHouseBuilder) Build() House {
+	return b.house
+}
+
+// func main() {
+// 	builder := NewHouseBuilder()
+// 	house := builder.SetWalls("Brick").SetRoof("Tile").SetFloors(2).SetGarage(true).SetSwimmingPool(true).Build()
+// 	fmt.Printf("House: %+v\n", house)
+// }
