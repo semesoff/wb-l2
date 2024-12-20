32ddc49b1eb076fae2825c01f41b1312ebf1a3a7
Thanos Loise <thanoslo@ga-e4.msk.21-school.ru>
2024-12-20 23:19:46 +0300

    Add L.2.3

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +24,85 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func unpackString1(s string) (string, error) {
+	prevChar := '\000'
+	res := strings.Builder{}
 
+	for _, char := range s {
+		if unicode.IsDigit(char) {
+			if prevChar == '\000' {
+				return "", fmt.Errorf("invalid string")
+			} else {
+				res.WriteString(strings.Repeat(string(prevChar), int(char-'0')))
+				prevChar = '\000'
+			}
+		} else {
+			if prevChar == '\000' {
+				prevChar = char
+			} else {
+				res.WriteRune(prevChar)
+				prevChar = char
+			}
+		}
+	}
+
+	if prevChar != '\000' {
+		res.WriteRune(prevChar)
+	}
+
+	return res.String(), nil
+}
+
+func unpackString(s string) (string, error) {
+	prevChar := '\000'
+	res := strings.Builder{}
+	isEscape := false
+	count := 0
+
+	for i, char := range s {
+		if isEscape {
+			res.WriteRune(char)
+			prevChar = char
+			isEscape = false
+		} else if unicode.IsDigit(char) {
+			num := int(char - '0')
+			count++
+
+			if i > 0 {
+				if prevChar == '\000' {
+					return "", fmt.Errorf("invalid string")
+				} else {
+					res.WriteString(strings.Repeat(string(prevChar), num-1))
+					prevChar = '\000'
+				}
+			} else {
+				res.WriteRune(char)
+			}
+
+		} else if char == '\\' {
+			isEscape = true
+		} else {
+			prevChar = char
+			res.WriteRune(char)
+		}
+	}
+
+	if s == "" {
+		return "", nil
+	}
+	if res.Len() == count {
+		return "", fmt.Errorf("invalid string")
+	}
+
+	return res.String(), nil
+}
+
+func main() {
+	fmt.Println(unpackString("a4bc2d5e"))
+	fmt.Println(unpackString("abcd"))
+	fmt.Println(unpackString("45"))
+	fmt.Println(unpackString(""))
+	fmt.Println(unpackString("qwe\\4\\5"))
+	fmt.Println(unpackString("qwe\\45"))
+	fmt.Println(unpackString("qwe\\\\5"))
 }
