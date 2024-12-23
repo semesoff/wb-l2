ec2d874e762effddd081ef1af1243ada01bdc9e1
Thanos Loise <thanoslo@ga-e4.msk.21-school.ru>
2024-12-24 00:22:15 +0300

    dev06 is completed.

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +21,113 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Options store flags values
+type Options struct {
+	fields       string
+	delimiter    string
+	separated    bool
+	filedIndexes []int
+}
 
+// parseFlags parses command line flags and returns Options
+func parseFlags() (Options, error) {
+	var options Options
+	// set flags
+	flag.StringVar(&options.fields, "f", "", "fields")
+	flag.StringVar(&options.delimiter, "d", "\t", "delimiter")
+	flag.BoolVar(&options.separated, "s", false, "separated")
+	// parse flags
+	flag.Parse()
+	// parse fields into fieldIndexes
+	var err error
+	options.filedIndexes, err = parseFields(options.fields)
+	return options, err
+}
+
+// readStdin reads lines from stdin and returns them as slice of strings
+func readStdin() ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(os.Stdin)
+	// iter over lines
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+// выбираем столбцы в строках по индексам
+func selectColumns(columns []string, indexes []int) []string {
+	// если оден столбец - возвращаем его
+	if len(columns) == 1 {
+		return columns
+	}
+	var selected []string
+	// проходимся по индексам и проверяем, что индекс меньше длины строки
+	for _, index := range indexes {
+		if index < len(columns) {
+			selected = append(selected, columns[index])
+		}
+	}
+	return selected
+}
+
+// основная функция обработки строк
+func cut(lines []string, options Options) []string {
+	var result []string
+	for _, line := range lines {
+		// если флаг -s, то проверяем наличие разделителя, если его нет, то пропускаем строку
+		if options.separated && !strings.Contains(line, options.delimiter) {
+			continue
+		}
+		// разбиваем строку на столбцы по options.delimiter
+		columns := strings.Split(line, options.delimiter)
+		// выбираем нужные столбцы по индексам
+		selectedColumns := selectColumns(columns, options.filedIndexes)
+		// добавляем в результат строку из выбранных столбцов
+		result = append(result, strings.Join(selectedColumns, options.delimiter))
+	}
+	return result
+}
+
+// parseFields
+func parseFields(fields string) ([]int, error) {
+	var indexes []int
+	for _, field := range strings.Split(fields, ",") {
+		var index int
+		fmt.Sscanf(field, "%d", &index)
+		if index == 0 {
+			return nil, fmt.Errorf("invalid field index: %s", field)
+		}
+		// индексы начинаются с 1
+		index--
+		indexes = append(indexes, index)
+	}
+	return indexes, nil
+}
+
+func main() {
+	// parse flags
+	options, err := parseFlags()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// read input from stdin
+	lines, err := readStdin()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// process
+	fmt.Println(lines, options)
+	result := cut(lines, options)
+
+	// print result
+	for _, line := range result {
+		fmt.Println(line)
+	}
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,153 @@
 package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestCut(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		options  Options
+		expected string
+	}{
+		{
+			name:     "select first column",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "1"},
+			expected: "col1\ncol4\ncol7",
+		},
+		{
+			name:     "select first and third columns",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "1,3"},
+			expected: "col1\tcol3\ncol4\tcol6\ncol7\tcol9",
+		},
+		{
+			name:     "select columns with custom delimiter",
+			input:    "col1,col2,col3\ncol4,col5,col6\ncol7,col8,col9",
+			options:  Options{fields: "2", delimiter: ","},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with separated flag",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "2", separated: true},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select second column with space delimiter",
+			input:    "col1 col2 col3\ncol4 col5 col6\ncol7 col8 col9",
+			options:  Options{fields: "2", delimiter: " "},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select non-existent column",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "4"},
+			expected: "\n\n",
+		},
+		{
+			name:     "select first column with no delimiter",
+			input:    "col1\ncol2\ncol3",
+			options:  Options{fields: "1"},
+			expected: "col1\ncol2\ncol3",
+		},
+		{
+			name:     "select first column with custom delimiter and separated flag",
+			input:    "col1,col2,col3\ncol4,col5,col6\ncol7,col8,col9",
+			options:  Options{fields: "1", delimiter: ",", separated: true},
+			expected: "col1\ncol4\ncol7",
+		},
+		{
+			name:     "select multiple columns with custom delimiter",
+			input:    "col1,col2,col3\ncol4,col5,col6\ncol7,col8,col9",
+			options:  Options{fields: "1,3", delimiter: ","},
+			expected: "col1,col3\ncol4,col6\ncol7,col9",
+		},
+		{
+			name:     "select columns with mixed delimiters",
+			input:    "col1,col2\tcol3\ncol4,col5\tcol6\ncol7,col8\tcol9",
+			options:  Options{fields: "2", delimiter: ","},
+			expected: "col2\tcol3\ncol5\tcol6\ncol8\tcol9",
+		},
+		{
+			name:     "select columns with tab delimiter and separated flag",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "2", delimiter: "\t", separated: true},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with space delimiter and separated flag",
+			input:    "col1 col2 col3\ncol4 col5 col6\ncol7 col8 col9",
+			options:  Options{fields: "2", delimiter: " ", separated: true},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with tab delimiter and no separated flag",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "2", delimiter: "\t"},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with space delimiter and no separated flag",
+			input:    "col1 col2 col3\ncol4 col5 col6\ncol7 col8 col9",
+			options:  Options{fields: "2", delimiter: " "},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with mixed delimiters and no separated flag",
+			input:    "col1,col2\tcol3\ncol4,col5\tcol6\ncol7,col8\tcol9",
+			options:  Options{fields: "2", delimiter: ","},
+			expected: "col2\tcol3\ncol5\tcol6\ncol8\tcol9",
+		},
+		{
+			name:     "select columns with mixed delimiters and separated flag",
+			input:    "col1,col2\tcol3\ncol4,col5\tcol6\ncol7,col8\tcol9",
+			options:  Options{fields: "2", delimiter: ",", separated: true},
+			expected: "col2\tcol3\ncol5\tcol6\ncol8\tcol9",
+		},
+		{
+			name:     "select columns with custom delimiter and no separated flag",
+			input:    "col1,col2,col3\ncol4,col5,col6\ncol7,col8,col9",
+			options:  Options{fields: "2", delimiter: ","},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with custom delimiter and separated flag",
+			input:    "col1,col2,col3\ncol4,col5,col6\ncol7,col8,col9",
+			options:  Options{fields: "2", delimiter: ",", separated: true},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with tab delimiter and no separated flag",
+			input:    "col1\tcol2\tcol3\ncol4\tcol5\tcol6\ncol7\tcol8\tcol9",
+			options:  Options{fields: "2", delimiter: "\t"},
+			expected: "col2\ncol5\ncol8",
+		},
+		{
+			name:     "select columns with space delimiter and no separated flag",
+			input:    "col1 col2 col3\ncol4 col5 col6\ncol7 col8 col9",
+			options:  Options{fields: "2", delimiter: " "},
+			expected: "col2\ncol5\ncol8",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Инициализируем fieldIndexes перед вызовом cut
+			if test.options.delimiter == "" {
+				test.options.delimiter = "\t"
+			}
+			test.options.filedIndexes, _ = parseFields(test.options.fields)
+			lines := strings.Split(test.input, "\n")
+			result := cut(lines, test.options)
+			output := strings.Split(test.expected, "\n")
+			if !reflect.DeepEqual(result, output) {
+				t.Errorf("expected %v, got %v", output, result)
+			}
+		})
+	}
+}
